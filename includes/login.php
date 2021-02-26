f388c81e2ccf5d30e22d8b1d37066b8415060a57
Kamil Wenta <gamerplrecording@gmail.com>
2021-02-26 09:45:48 +0100

    Add login screen

---
+++ includes/login.php
@@ -0,0 +1,17 @@
+<div class="startContainer">
+  <div class="logo">
+    <h1>eDziennik</h1>
+  </div>
+  <div class="form">
+    <input type="text" name="email" placeholder="E-mail">
+    <input type="password" name="password" placeholder="Password">
+    <button type="submit" class="signin">Zaloguj się</button>
+    <a href="">Nie pamiętam hasła</a>
+  </div>
+  <div class="register">
+    <p>Nie masz konta?</p>
+    <a href="">Zarejestruj się!</a>
+  </div>
+  <div id="leftImage"><img src="/assets/img/start_image1.svg" alt="School image" class="startImage"></div>
+  <div id="rightImage"><img src="/assets/img/start_image2.svg" alt="School image" class="startImage"></div>
+</div>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+include 'includes/header.php';
+include 'includes/login.php';
+include 'includes/footer.php';
