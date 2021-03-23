7d9f2034fa9f06c7223b597cce1280357aad5b44
Kamil Wenta <gamerplrecording@gmail.com>
2021-03-23 12:37:33 +0100

    Add editUser function
    Fix deactivateUser function

--- functions/deactivateUser.php
+++ functions/deactivateUser.php
@@ -1,21 +1,19 @@
 <?php
-require '../functions/isLoggedIn.php';
+session_start();
+if (!isset($_SESSION["user"]) && $_SESSION['user']['rank'] !== "admin") {
+  header("Location: http://{$_SERVER['HTTP_HOST']}/");
+  exit();
+}
 require "../db.php";
 $conn = connectToDB();
 
-$rank = isset($_POST['class']) ? "students" : "teachers";
-$sql = "SELECT `id` FROM {$rank} WHERE `id`={$_POST['typeid']} AND `user_id`={$_POST['id']} AND `class`='{$_POST['class']}'";
-$exist = mysqli_query($conn, $sql);
+$sql = "SELECT `isActivated` FROM users WHERE `id`={$_POST['id']} AND `email`='{$_POST['email']}' AND `isActivated`={$_POST['isActivated']}";
+$isActivated = mysqli_query($conn, $sql);
 
-if ($exist->num_rows !== 0) {
-  $sql = "SELECT `isActivated` FROM users WHERE `id`={$_POST['id']} AND `email`='{$_POST['email']}' AND `isActivated`={$_POST['isActivated']}";
-  $isActivated = mysqli_query($conn, $sql);
-
-  if ($isActivated->num_rows !== 0) {
-    $newValue = $_POST['isActivated'] === "0" ? 1 : 0;
-    $sql = "UPDATE users SET `isActivated` = {$newValue} WHERE `id`={$_POST['id']} AND `email`='{$_POST['email']}'";
-    mysqli_query($conn, $sql);
-  }
+if ($isActivated->num_rows !== 0) {
+  $newValue = $_POST['isActivated'] === "0" ? 1 : 0;
+  $sql = "UPDATE users SET `isActivated` = {$newValue} WHERE `id`={$_POST['id']} AND `email`='{$_POST['email']}'";
+  mysqli_query($conn, $sql);
 }
 
 header("Location: http://{$_SERVER['HTTP_HOST']}/admin/users.php");
--- functions/editUser.php
+++ functions/editUser.php
@@ -1,9 +1,21 @@
 <?php
-require '../functions/isLoggedIn.php';
+session_start();
+if (!isset($_SESSION["user"]) && $_SESSION['user']['rank'] !== "admin") {
+  header("Location: http://{$_SERVER['HTTP_HOST']}/");
+  exit();
+}
 require "../db.php";
 $conn = connectToDB();
 
-die("też działa");
+$sql = "SELECT `id` FROM users WHERE `id`={$_POST['id']} AND `email`='{$_POST['email']}'";
+$exist = mysqli_query($conn, $sql);
 
+$rank = isset($_POST['class']) ? "students" : "teachers";
+$lastField = isset($_POST['class']) ? "`class`='{$_POST['class']}'" : "`phone`='{$_POST['phone']}'";
+
+if ($exist->num_rows !== 0) {
+  $sql = "UPDATE {$rank} SET `first_name` = '{$_POST['first_name']}', `last_name`= '{$_POST['last_name']}', {$lastField} WHERE `id`={$_POST['typeid']} AND `user_id`={$_POST['id']}";
+  mysqli_query($conn, $sql);
+}
 
 header("Location: http://{$_SERVER['HTTP_HOST']}/admin/users.php");
