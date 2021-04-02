1dca769ea3140b63cdb22e104bbd234f5bf9e57c
Kamil Wenta <gamerplrecording@gmail.com>
2021-04-02 22:55:18 +0200

    Add deleteNote function

---
+++ functions/deleteNote.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+if (!isset($_SESSION["user"]) || (isset($_SESSION["user"]) && $_SESSION['user']['rank'] !== "teacher")) {
+  header("Location: http://{$_SERVER['HTTP_HOST']}/");
+  exit();
+}
+
+require "../db.php";
+$conn = connectToDB();
+$sql = sprintf(
+  "DELETE FROM notes WHERE `teacher_id`=%s AND `student_id`=%s AND `id`=%s",
+  $_SESSION['user']['id'],
+  mysqli_real_escape_string($conn, $_POST['student_id']),
+  mysqli_real_escape_string($conn, $_POST['note_id'])
+);
+mysqli_query($conn, $sql);
+if (mysqli_affected_rows($conn) > 0) {
+  $_SESSION['snackalert'] = ["type" => "success", "text" => "Uwaga została usunięta"];
+} else {
+  $_SESSION['snackalert'] = ["type" => "error", "text" => "Nie udało się usunąć uwagi"];
+};
+
+header("Location: http://{$_SERVER['HTTP_HOST']}/teacher/notes.php");
