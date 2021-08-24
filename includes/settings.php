7f6d219f53cceedb1a73f4b24f0215a162fa7229
Kamil Wenta <kamil.wenta222@gmail.com>
2021-08-24 16:59:51 +0200

    Translate form placeholders

--- includes/settings.php
+++ includes/settings.php
@@ -5,13 +5,13 @@
     <div class="settings__forms">
       <form action="../functions/editPasword.php" class="form form--settings" method="POST">
         <input type="email" name="email" placeholder="E-Mail" autocomplete="email" hidden>
-        <input type="password" name="currentPassword" placeholder="Aktualne hasło" autocomplete="current-password" required>
-        <input type="password" name="newPassword" placeholder="Nowe hasło" minlength="8" maxlength="32" autocomplete="new-password" required>
+        <input type="password" name="currentPassword" placeholder="Current password" autocomplete="current-password" required>
+        <input type="password" name="newPassword" placeholder="New password" minlength="8" maxlength="32" autocomplete="new-password" required>
         <button class="form__submit form__submit--insert form__submit--newPassword" type="submit">Change password</button>
       </form>
       <form action="../functions/editEmail.php" class="form form--settings" method="POST">
         <input type="email" name="currentEmail" value="<?php echo $_SESSION['user']['email'] ?>" disabled>
-        <input type="email" name="email" placeholder="Nowy E-Mail" autocomplete="email" required>
+        <input type="email" name="email" placeholder="New E-Mail" autocomplete="email" required>
         <button class="form__submit form__submit--insert form__submit--newPassword" type="submit">Change E-Mail</button>
       </form>
     </div>
